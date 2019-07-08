0751b67ccbc4bcaef3f9b914df87d7f428baef7f
Rehten <Mroow@yandex.ru>
2019-07-08 21:22:12 +0300

    GB_OOP part01_01 complete

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <cmath>
+
+namespace GBLesson {
+    class Power
+    {
+    private:
+        int val;
+        int coef;
+    public:
+        void set (int value, int power)
+        {
+            val = value;
+            coef = power;
+        }
+        void calculate ()
+        {
+            std::cout << pow(val, coef) << std::endl;
+        }
+        Power() = default;
+    };
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    GBLesson::Power power = *new GBLesson::Power();
+    power.set(2, 3);
+    power.calculate();
+    power.set(3, 3);
+    power.calculate();
+    power.set(4, 2);
+    power.calculate();
+    power.set(5, 5);
+    power.calculate();
     return 0;
 }
