b572830b2ad2eac8ccbdef091ad65c4c70cb51e8
Rehten <Mroow@yandex.ru>
2019-07-11 00:32:17 +0300

    GB_OOP part02_01 uncomplete - let make random

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using namespace std;
+
+class rand_generator
+{
+private:
+    mutable int cur_num{-1};
+public:
+    int current() const {
+        if (cur_num == -1)
+        {
+            cout << "Error!!! Start random is not generated";
+        }
+        return cur_num;
+    };
+    int next() const {
+        cur_num = cur_num + 1;
+
+        return cur_num;
+    };
+    rand_generator() = default;
+    rand_generator(int num): cur_num(num) {};
+    rand_generator(rand_generator& src): cur_num(src.current()) {};
+};
+
+int main()
+{
+    rand_generator rndm(23);
+    for (int i = 0; i != 10; i++)
+    {
+        cout << rndm.current() << ((i != 9) ? " -> " : "");
+        rndm.next();
+    }
     return 0;
 }
