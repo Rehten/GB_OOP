04831b887fb934d342749e4ea2160777ffc377f4
Rehten <Mroow@yandex.ru>
2019-07-11 00:46:02 +0300

    GB_OOP part02_01 uncomplete - let make random

--- main.cpp
+++ main.cpp
@@ -4,35 +4,67 @@
 
 using namespace std;
 
-class rand_generator
+class Person
+{
+protected:
+    int name{1};
+    int age{2};
+    int sex{3};
+    int weight{4};
+public:
+    int get_name() const
+    {
+        return name;
+    };
+    void set_name(int new_name)
+    {
+        name = new_name;
+    };
+    int get_age() const
+    {
+        return age;
+    };
+    void set_age(int new_age)
+    {
+        age = new_age;
+    };
+    int get_sex() const
+    {
+        return sex;
+    };
+    void set_sex(int new_sex)
+    {
+        sex = new_sex;
+    };
+    int get_weight() const
+    {
+        return weight;
+    };
+    void set_weight(int new_weight)
+    {
+        weight = new_weight;
+    };
+};
+
+class Student: public Person
 {
 private:
-    mutable int cur_num{-1};
+    int age_studying{5};
 public:
-    int current() const {
-        if (cur_num == -1)
-        {
-            cout << "Error!!! Start random is not generated";
-        }
-        return cur_num;
+    int get_age_studying() const
+    {
+        return age_studying;
     };
-    int next() const {
-        cur_num = cur_num + 1;
-
-        return cur_num;
+    void set_age_studying(int new_age_studying)
+    {
+        age_studying = new_age_studying;
     };
-    rand_generator() = default;
-    rand_generator(int num): cur_num(num) {};
-    rand_generator(rand_generator& src): cur_num(src.current()) {};
 };
 
 int main()
 {
-    rand_generator rndm(23);
-    for (int i = 0; i != 10; i++)
-    {
-        cout << rndm.current() << ((i != 9) ? " -> " : "");
-        rndm.next();
-    }
+    Student student;
+    cout << student.get_age() << " " << student.get_name() << " " << student.get_sex() << " " << student.get_weight()
+    << " " << student.get_age_studying() << endl;
     return 0;
 }
